a5520ad81d6a86349b2e3f7f99ffbfae9b86e530
xpleonix <96414736+xpleonix@users.noreply.github.com>
2022-07-05 13:04:14 +0300

    Add header and buttons

--- ABCalculator.py
+++ ABCalculator.py
@@ -1,9 +1,27 @@
 
 import tkinter as tk
 
+# function to close program
+def do_close():
+    root.destroy()
+
 # main window
 root = tk.Tk()
 root.geometry("280x300")
 root.title("A/B Calculator")
+
+
+# header
+lblTitle = tk.Label(text="A/B Calculator", font=('Helvetica', 16, 'bold'), fg='#0000cc')
+lblTitle.place(x=55, y=20)
+
+# close button
+btnProcess = tk.Button(root, text="Calculate", font=('Helvetica', 10, 'bold'))
+btnProcess.place(x=25, y=250, width=90, height=30)
+
+# close button
+btnClose = tk.Button(root, text="Close", font=('Helvetica', 10, 'bold'), command=do_close)
+btnClose.place(x=160, y=250, width=90, height=30)
+
 
 root.mainloop()
