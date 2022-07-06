c568cfb267c886de202cd08cab29aade1d9896da
xpleonix <96414736+xpleonix@users.noreply.github.com>
2022-07-06 22:57:15 +0300

    Add initial value for entries

--- ABCalculator.py
+++ ABCalculator.py
@@ -31,14 +31,16 @@ lblTitle1.place(x=25, y=55)
 lblVisitors1 = tk.Label(text="Visitors", font=('Helvetica', 10, 'bold'))
 lblVisitors1.place(x=25, y=85)
 
-entVisitors1 = tk.Entry(font=('Helvetica', 10, 'bold'))
+entVisitors1 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
 entVisitors1.place(x=115, y=85, width=90, height=20)
+entVisitors1.insert(tk.END, '0')
 
 lblConversions1 = tk.Label(text="Conversions", font=('Helvetica', 10, 'bold'))
 lblConversions1.place(x=25, y=115)
 
-entConversions1 = tk.Entry(font=('Helvetica', 10, 'bold'))
+entConversions1 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
 entConversions1.place(x=115, y=115, width=90, height=20)
+entConversions1.insert(tk.END, '0')
 
 
 # header for test group
@@ -49,14 +51,16 @@ lblTitle1.place(x=25, y=145)
 lblVisitors2 = tk.Label(text="Visitors", font=('Helvetica', 10, 'bold'))
 lblVisitors2.place(x=25, y=175)
 
-entVisitors2 = tk.Entry(font=('Helvetica', 10, 'bold'))
+entVisitors2 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
 entVisitors2.place(x=115, y=175, width=90, height=20)
+entVisitors2.insert(tk.END, '0')
 
 lblConversions2 = tk.Label(text="Conversions", font=('Helvetica', 10, 'bold'))
 lblConversions2.place(x=25, y=205)
 
-entConversions2 = tk.Entry(font=('Helvetica', 10, 'bold'))
+entConversions2 = tk.Entry(font=('Helvetica', 10, 'bold'), justify='center')
 entConversions2.place(x=115, y=205, width=90, height=20)
+entConversions2.insert(tk.END, '0')
 
 
 # close button
